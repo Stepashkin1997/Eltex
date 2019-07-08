b1579724a6938fcd3c660aeb49dbba6d00f79bc1
Nikita <33stepashkin33@gmail.com>
2019-07-08 20:48:45 +0700

    Class Credentials has modified

--- src/ru/eltex/app/java/lab2/Credentials.java
+++ src/ru/eltex/app/java/lab2/Credentials.java
@@ -9,4 +9,32 @@ public class Credentials {
     private StringBuilder surname;
     private StringBuilder thirdname;
     private StringBuilder email;
+
+    public Credentials(StringBuilder name, StringBuilder surname, StringBuilder thirdname, StringBuilder email) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.surname = surname;
+        this.thirdname = thirdname;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public StringBuilder getName() {
+        return name;
+    }
+
+    public StringBuilder getSurname() {
+        return surname;
+    }
+
+    public StringBuilder getThirdname() {
+        return thirdname;
+    }
+
+    public StringBuilder getEmail() {
+        return email;
+    }
 }
