4bde0f9ffd45ddaf83d9ba904d5902ee9061a6b7
Nick Chi <xeritt@gmail.com>
2019-07-08 17:37:16 +0700

    правки

--- src/ru/eltex/app/java/Main.java
+++ src/ru/eltex/app/java/Main.java
@@ -7,32 +7,29 @@ public class Main {
         int count = Integer.parseInt(args[1]);
 
 
-        ICrudAction action;
+        ICrudAction action = null;
+        for (int i=0;i<count;i++) {
 
-        switch (TYPE) {
-            case "Coffee": {
-                action = new Coffee();//создание объекта типа Coffee
-                break;
+            switch (TYPE) {
+                case "Coffee": {
+                    action = new Coffee();//создание объекта типа Coffee
+                    break;
+                }
+                case "Tea": {
+                    action = new Tea();//создание объекта типа Tea
+                    break;
+                }
+                default: {
+                    System.err.println("Wrong object name");//выод ошибки
+                    return;
+                }
             }
-            case "Tea": {
-                action = new Tea();//создание объекта типа Tea
-                break;
-            }
-            default: {
-                System.err.println("Wrong object name");//выод ошибки
-                return;
-            }
-        }
-
-        for (int i=0;i<count;i++){
             action.create();
+            System.out.println(Drinks.count);
+            action.update();
+            action.read();
+            action.delete();
+            action.read();
         }
-
-        System.out.println(Drinks.count);
-
-        action.update();
-        action.read();
-        action.delete();
-        action.read();
     }
 }
