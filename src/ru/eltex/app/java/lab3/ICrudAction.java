6d7d4865cda01c2a3dfd6a723192da657d987671
Nikita <33stepashkin33@gmail.com>
2019-07-09 20:24:04 +0700

    Init lab3

---
+++ src/ru/eltex/app/java/lab3/ICrudAction.java
@@ -0,0 +1,18 @@
+package ru.eltex.app.java.lab3;
+
+import java.io.IOException;
+
+public interface ICrudAction {
+
+    /*заполнение объекта случайными значениями и инкремент счётчика*/
+    void create();
+
+    /*вывод данных на экран*/
+    void read();
+
+    /*ввод данных с клавиатуры*/
+    void update() throws IOException;
+
+    /*принудительное зануление данных в объекте и декремент счетчика*/
+    void delete();
+}
---
+++ src/ru/eltex/app/java/lab3/Main.java
@@ -0,0 +1,9 @@
+package ru.eltex.app.java.lab3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+    }
+}
